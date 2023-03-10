d1ce6dfcf591dd2992967f49a30ea88d0446725f
Elorriaga <pedro.elorriaga@accenture.com>
2023-03-10 16:39:18 -0300

    arrumarTimer

--- zExercicios/Udemy/exercicio10Timer/main.js
+++ zExercicios/Udemy/exercicio10Timer/main.js
@@ -1,3 +1,40 @@
-function start(){
-    
+function relogio(start, stop, reset) {
+    let resultado = document.querySelector('.timer_zone')
+
+    let segundos = 0;
+    let timer
+
+    let start = start()
+
+    function createSeconds(segundos) {
+        const data = new Date(segundos * 1000)
+
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'
+        })
+    }
+
+    function start() {
+        clearInterval(timer)
+        resultado.style.color = 'black'
+        timer = setInterval(function () {
+            segundos++
+            resultado.innerHTML = createSeconds(segundos)
+
+        }, 1000)
+
+    }
+
+    function stop() {
+        clearInterval(timer)
+        resultado.style.color = 'red'
+    }
+
+    function reset() {
+        clearInterval(timer)
+        resultado.innerHTML = '00:00:00'
+        segundos = 0
+        resultado.style.color = 'black'
+    }
 }
