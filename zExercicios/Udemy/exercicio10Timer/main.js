135775d9b1f266a51e2be56f95dbd197a63df5e0
Elorriaga <pedro.elorriaga@accenture.com>
2023-03-14 10:34:58 -0300

    envio14/03

--- zExercicios/Udemy/exercicio10Timer/main.js
+++ zExercicios/Udemy/exercicio10Timer/main.js
@@ -1,40 +1,35 @@
-function relogio(start, stop, reset) {
-    let resultado = document.querySelector('.timer_zone')
+let resultado = document.querySelector('.timer_zone')
 
-    let segundos = 0;
-    let timer
+let segundos = 0;
+let timer
 
-    let start = start()
+function createSeconds(segundos) {
+    const data = new Date(segundos * 1000)
 
-    function createSeconds(segundos) {
-        const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+}
 
-        return data.toLocaleTimeString('pt-BR', {
-            hour12: false,
-            timeZone: 'UTC'
-        })
-    }
+function start() {
+    clearInterval(timer)
+    resultado.style.color = 'black'
+    timer = setInterval(function () {
+        segundos++
+        resultado.innerHTML = createSeconds(segundos)
 
-    function start() {
-        clearInterval(timer)
-        resultado.style.color = 'black'
-        timer = setInterval(function () {
-            segundos++
-            resultado.innerHTML = createSeconds(segundos)
+    }, 1000)
+}
 
-        }, 1000)
+function stop() {
+    clearInterval(timer)
+    resultado.style.color = 'red'
+}
 
-    }
-
-    function stop() {
-        clearInterval(timer)
-        resultado.style.color = 'red'
-    }
-
-    function reset() {
-        clearInterval(timer)
-        resultado.innerHTML = '00:00:00'
-        segundos = 0
-        resultado.style.color = 'black'
-    }
+function reset() {
+    clearInterval(timer)
+    resultado.innerHTML = '00:00:00'
+    segundos = 0
+    resultado.style.color = 'black'
 }
