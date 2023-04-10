b74d67dcdc15a43515e7082e1c742f2803d56148
PedroElorriaga <pedrohes2002@outlook.com>
2023-04-10 10:38:54 -0300

    envio 10/04/23

--- xxAvancado/Seção4/CalculadoraFuncs/main.js
+++ xxAvancado/Seção4/CalculadoraFuncs/main.js
@@ -8,22 +8,33 @@ function iniciaCalculadora() {
 
         inicia() {
             this.getButtons() // ESSE THIS É O OBEJTO startCalculadora
+            this.display.focus()
         },
 
         getButtons() {
             document.addEventListener('click', (e) => {
-                const el  = e.target
-                
-                if (el.classList.contains('btn-num')){
+                const el = e.target
+
+                if (el.classList.contains('btn-num')) {
                     this.adicionarNumberDisplay(el.innerText) // ESSE THIS PASSA A SER O DOCUMENT SE MINHA FUNÇÃO NÃO FOSSE ARROW
                 }
 
-                if (el.classList.contains('btn-clear')){
+                if (el.classList.contains('btn-clear')) {
                     this.botaoLimpar()
-                } 
+                }
 
-                if (el.classList.contains('btn-pop')){
+                if (el.classList.contains('btn-pop')) {
                     this.botaoLimparPop(this.display.value)
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.fazerConta()
+                }
+            })
+
+            this.display.addEventListener('keypress', (e) => {
+                if (e.keyCode === 13) { 
+                    this.fazerConta()
                 }
             })
         },
@@ -39,6 +50,41 @@ function iniciaCalculadora() {
         botaoLimparPop(valor) {
             const remDisplay = valor.slice(0, -1)
             this.display.value = remDisplay
+        },
+
+        fazerConta() {
+            // let operador = this.display.value
+
+            // if (operador.includes('+')) {
+            //     operador = operador.split('+')
+
+            //     let soma = 0
+            //     for (let chave in operador) {
+            //         let inteiro = Number(operador[chave])
+            //         soma += inteiro
+            //     }
+            //     this.display.value = soma
+            // }
+
+
+            // FORMA MAIS SIMPLIFICADA
+            let operacao = this.display.value
+
+            try {
+                operacao = eval(operacao) // EVAL É UTILIZADO PARA O JAVASCRIPT FAZER AS FUÇÕES SOZINHO
+
+                if (!operacao) {
+                    alert('Conta invalida!')
+                    this.botaoLimpar()
+                    return
+                }
+
+                this.display.value = operacao
+            } catch (e) {
+                window.alert('Conta invalida!')
+                this.botaoLimpar()
+            }
+
         }
     }
 }
