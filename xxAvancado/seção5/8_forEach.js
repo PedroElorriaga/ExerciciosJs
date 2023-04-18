c6c218a69910aeea24b0c7947cf2a4ff236f6649
PedroElorriaga <pedrohes2002@outlook.com>
2023-04-18 19:31:40 -0300

    correção forEach

--- xxAvancado/seção5/8_forEach.js
+++ xxAvancado/seção5/8_forEach.js
@@ -4,7 +4,7 @@ valores.forEach((valor) => {
     console.log(valor)
 })
 
-
+console.log('---------------------')
 
 // COPIANDO O REDUCE
 const valoresDois = [10, 30, 2, 3, 6, 7, 8, 11, 35]
@@ -14,5 +14,4 @@ let recebeValores = 0
 valoresDois.forEach((valor) => {
     recebeValores += valor
 })
-console.log('---------------------')
 console.log(recebeValores)
