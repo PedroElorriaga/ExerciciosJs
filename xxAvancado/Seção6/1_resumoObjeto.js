5fb04bcda8c57c1a9bdadd177ce1bd45d58688cf
Pedro Elorriaga <elorriaga@Pedros-MacBook-Pro.local>
2023-04-19 19:03:05 -0300

    envio MacBook pro 19/04/23

---
+++ xxAvancado/Seção6/1_resumoObjeto.js
@@ -0,0 +1,10 @@
+// RESUMO DO OBJETO
+
+// COMO CRIAR UM OBJETO DINAMICAMENTE
+const pessoa = new Object()
+pessoa.nome = 'Pedro'
+pessoa.sobrenome = 'Elorriaga'
+
+console.log(pessoa) // RETORNA {nome: 'Pedro', sobrenome: 'Elorriaga'}
+
+console.log(pessoa.nome) // RETORNA Pedro
