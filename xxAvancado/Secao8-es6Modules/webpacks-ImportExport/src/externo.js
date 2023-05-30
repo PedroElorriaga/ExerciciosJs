c8e605e49598b6ac2f55c6648a4564c67f79c97a
PedroElorriaga <pedrohes2002@outlook.com>
2023-05-30 19:48:57 -0300

    envio 30/05/23

---
+++ xxAvancado/Secao8-es6Modules/webpacks-ImportExport/src/externo.js
@@ -0,0 +1,3 @@
+export const nome = 'Pedro'
+export const idade = 20
+
--- xxAvancado/Secao8-es6Modules/webpacks-ImportExport/src/index.js
+++ xxAvancado/Secao8-es6Modules/webpacks-ImportExport/src/index.js
@@ -1,6 +1,7 @@
-//
-//
-//
-//
-//
-console.log('Testando DEVTOOL = SOURCE-MAP')
+import { nome as nomeExterno, idade } from './externo.js'
+
+
+const nome = 'Luiz'
+
+console.log('Nome interno: ' + nome)
+console.log('Nome externo: ' + nomeExterno)
