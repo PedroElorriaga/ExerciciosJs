d470663bfd8d1bdd1f0395001e8ddb95e0bda2b4
PedroElorriaga <pedrohes2002@outlook.com>
2023-06-14 22:09:08 -0300

    envio 14/06/23

--- xxAvancado/Seção10-Nodejs/Express/Aula_8_MongoDB/server.js
+++ xxAvancado/Seção10-Nodejs/Express/Aula_8_MongoDB/server.js
@@ -1,7 +1,20 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const routes = require('./routes') // IMPORTAÇÃO DE ROUTES
 const path = require('path')
+
+// ------------- CONECTANDO NA BASE DE DADOS --------------
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Conectado na base de dados...')
+        app.emit('connect')
+    })
+    .catch(e => console.log(e))
+
 
 app.use(express.urlencoded({ extended: true }))
 
@@ -16,7 +29,9 @@ app.set('view engine', 'ejs')
 app.use(routes) // UTILIZANDO ROUTES COMO PARAMETRO
 
 
-app.listen(3000, () => {
-    console.log('Servidor sendo executado na porta: 3000')
-    console.log('Acessar http://localhost:3000')
+app.on('connect', () => {
+    app.listen(3000, () => {
+        console.log('Servidor sendo executado na porta: 3000')
+        console.log('Acessar http://localhost:3000')
+    })
 })
